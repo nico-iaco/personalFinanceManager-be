febeb617f51de6df54f79245797b284c640fe935
Nicola Iacovelli <nicolaiacovelli98@gmail.com>
2022-05-16 14:17:29 +0200

    Used builder to make db query

--- src/repository/user/userRepository.go
+++ src/repository/user/userRepository.go
@@ -3,10 +3,10 @@ package user
 import (
 	"context"
 	"github.com/kamva/mgm/v3"
+	"github.com/kamva/mgm/v3/builder"
 	"go.mongodb.org/mongo-driver/bson"
 	"log"
 	"personalFinanceManager/src/model"
-	"personalFinanceManager/src/utils"
 )
 
 func getUserCollection() *mgm.Collection {
@@ -28,7 +28,7 @@ func AddUser(user model.User) model.User {
 func GetUser(email string) model.User {
 	userCollection := getUserCollection()
 	var result model.User
-	filter := bson.D{{"email", utils.SanitizeString(email)}}
+	filter := builder.S(builder.Lookup(userCollection, "email", email, "email"))
 	err := userCollection.FindOne(context.Background(), filter).Decode(&result)
 	if err != nil {
 		log.Fatal(err)
@@ -40,7 +40,7 @@ func GetUser(email string) model.User {
 func GetUserById(id string) model.User {
 	userCollection := getUserCollection()
 	var result model.User
-	filter := bson.D{{"id", utils.SanitizeString(id)}}
+	filter := builder.S(builder.Lookup(userCollection, "id", id, "id"))
 	err := userCollection.FindOne(context.Background(), filter).Decode(&result)
 	if err != nil {
 		log.Fatal(err)
@@ -52,7 +52,7 @@ func GetUserById(id string) model.User {
 func UpdateUser(user model.User, updatedFields bson.D) model.User {
 	userCollection := getUserCollection()
 	var result model.User
-	filter := bson.D{{"id", user.ID}}
+	filter := builder.S(builder.Lookup(userCollection, "id", user.ID, "id"))
 	err := userCollection.FindOneAndUpdate(context.Background(), filter, updatedFields).Decode(&result)
 	log.Printf("Updated user %v", result)
 	if err != nil {
@@ -63,7 +63,7 @@ func UpdateUser(user model.User, updatedFields bson.D) model.User {
 
 func CheckEmailExists(email string) bool {
 	userCollection := getUserCollection()
-	filter := bson.D{{"email", utils.SanitizeString(email)}}
+	filter := builder.S(builder.Lookup(userCollection, "email", email, "email"))
 	err := userCollection.FindOne(context.Background(), filter).Err()
 	return err == nil
 }
